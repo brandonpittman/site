26af6e4eedbec3fa0136197818ec05cd2299cd7f
Brandon Pittman <blp@hey.com>
2021-04-07 17:06:44 +0900

    chore: 🔧 add RSS feed handling

--- src/pages/api/hey.ts
+++ src/pages/api/hey.ts
@@ -2,6 +2,10 @@ import Feed from "rss-to-json";
 import { NextApiResponse, NextApiRequest } from "next";
 
 module.exports = async (_req: NextApiRequest, res: NextApiResponse) => {
-  const rss = await Feed.load("https://world.hey.com/blp/feed.atom");
-  return res.json(rss);
+  try {
+    const rss = await Feed.load("https://world.hey.com/blp/feed.atom");
+    return res.json(rss);
+  } catch (e) {
+    return res.send("Error fetching feed.");
+  }
 };
