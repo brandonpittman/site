da68fd0cd46af08d161b57643897b14099a2883f
Brandon Pittman <blp@hey.com>
2021-05-11 09:50:01 +0900

    feat: ✨ expose getFeed function

--- src/pages/api/hey.ts
+++ src/pages/api/hey.ts
@@ -1,10 +1,12 @@
 import Feed from "rss-to-json";
 import { NextApiResponse, NextApiRequest } from "next";
 
+export const getFeed = async () =>
+  await Feed.load("https://world.hey.com/blp/feed.atom");
+
 module.exports = async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const rss = await Feed.load("https://world.hey.com/blp/feed.atom");
-    return res.json(rss);
+    return res.json(await getFeed());
   } catch (e) {
     return res.status(500).send("Error fetching feed.");
   }
