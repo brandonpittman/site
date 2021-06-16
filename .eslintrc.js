654074c158fbd17fde2d388537784fcd423e2801
Brandon Pittman <blp@hey.com>
2021-06-16 10:27:50 +0900

    chore: 🔧 update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,41 +8,31 @@ module.exports = {
       jsx: true,
     },
   },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
   env: {
     browser: true,
     amd: true,
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
+    "next",
+    "next/core-web-vitals",
     "plugin:prettier/recommended", // Make sure this is always the last element in the array.
   ],
   rules: {
-    "prefer-const": "off",
-    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "react/display-name": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "react/no-unescaped-entities": "off",
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        components: ["Link"],
-        specialLink: ["hrefLeft", "hrefRight"],
-        aspects: ["invalidHref", "preferButton"],
-      },
-    ],
+    // "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+    // "react/display-name": "off",
+    // "prefer-const": "off",
+    // "@typescript-eslint/explicit-function-return-type": "off",
+    // "@typescript-eslint/explicit-module-boundary-types": "off",
+    // "@typescript-eslint/no-explicit-any": "off",
+    //"react/no-unescaped-entities": "off",
+    // "jsx-a11y/anchor-is-valid": [
+    //   "error",
+    //   {
+    //     components: ["Link"],
+    //     specialLink: ["hrefLeft", "hrefRight"],
+    //     aspects: ["invalidHref", "preferButton"],
+    //   },
+    // ],
   },
 };
