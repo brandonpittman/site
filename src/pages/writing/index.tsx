32af14397ffaf88347be9fd946f6d58d24a1d0b6
Brandon Pittman <brandon@blp.is>
2021-09-13 10:03:36 +0900

    fix: 🐛 writing index list

--- src/pages/writing/index.tsx
+++ src/pages/writing/index.tsx
@@ -1,4 +1,4 @@
-import Article from "../../components/article";
+import Link from "next/link";
 import Layout from "../../components/layout";
 import { getAllPosts } from "../../lib/api";
 
@@ -18,12 +18,16 @@ export async function getStaticProps() {
 export default function Writing({ posts }) {
   return (
     <Layout className="max-w-3xl mx-auto" meta={meta}>
-      <h1 className="text-4xl font-bold">Recent Blog Posts</h1>
+      <h1 className="text-4xl font-bold">Writing</h1>
 
-      <ul className="mt-16 grid gap-16">
+      <ul className="mt-16 grid gap-4">
         {posts.map((post) => (
           <li key={post.data.slug} className="">
-            <Article post={post} />
+            <Link href="/writing/[slug]" as={"/writing/" + post.data.slug}>
+              <a className="font-bold text-gray-500 rounded-md focus:outline-none dark:focus:text-gray-100 focus:text-gray-900 hover:text-gray-900 dark:hover:text-gray-100">
+                {post.data.title}
+              </a>
+            </Link>
           </li>
         ))}
       </ul>
