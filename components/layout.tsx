5997942cdf55716a337cf7e02953d9837fef4c54
Brandon Pittman <brandon@blp.is>
2021-11-19 16:24:21 +0900

    fix flex

--- components/layout.tsx
+++ components/layout.tsx
@@ -21,8 +21,8 @@ export default function Layout({
   return (
     <>
       <SEO title={meta.title} description={meta.description} />
-      <div className="flex flex-col w-full min-h-screen dark:bg-black dark:text-gray-50">
-        <div className="container mx-auto">
+      <div className="dark:bg-black dark:text-gray-50">
+        <div className="container mx-auto min-h-screen flex flex-col flex-1">
           <a
             href="#skip-content-target"
             className="sr-only m-4 focus:not-sr-only focus:!fixed bg-white rounded-md z-50 shadow left-4 top-4 focus:ring hover:ring focus:outline-none !p-1"
