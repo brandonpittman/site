76f7c7b65412da5403810cc5bc5e164f33213ead
Brandon Pittman <brandon@blp.is>
2021-11-26 15:12:44 +0900

    fix types

--- hooks/use-shared-state.ts
+++ hooks/use-shared-state.ts
@@ -2,7 +2,7 @@ import useSWR from "swr";
 
 const useSharedState = (key: string, initial: any) => {
   const { data: state, mutate: setState } = useSWR(key, {
-    initialData: initial,
+    fallbackData: initial,
   });
   return [state, setState];
 };
