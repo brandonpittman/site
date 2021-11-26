5a8263c448ee80fc8968c80e6e362b11be0f2231
Brandon Pittman <brandon@blp.is>
2021-11-26 14:51:34 +0900

    Read unread from cache, then update cache

--- pages/api/pinboard/unread.ts
+++ pages/api/pinboard/unread.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Redis from "ioredis";
-// import { isWithinInterval, subMinutes, isBefore } from "date-fns/fp";
+import { auth, lrange, get, set, del } from "@upstash/redis";
 
 export type PinboardItem = {
   href: string;
@@ -17,29 +17,32 @@ export type PinboardItem = {
 const Pinboard = require("node-pinboard").default;
 const token = process.env.PINBOARD_TOKEN;
 const pinboard = new Pinboard(token);
-const client = new Redis(process.env.UPSTASH_BLOG);
-// const oneMinuteAgo = () => subMinutes(1)(new Date());
 
-// const withinOneMinute = (date: number) =>
-//   isWithinInterval(
-//     {
-//       start: oneMinuteAgo(),
-//       end: new Date(),
-//     },
-//     date
-//   );
+const authorizeUpstash = () =>
+  auth(
+    "https://apn1-known-marlin-30903.upstash.io",
+    "AXi3ACQgNzliYzBhMDctZWNlOC00ZjQ4LWIzOGItMzgwOWQ0ZDk0NzNiOGU2YjQ0ZDJlNWU0NDU4N2IxNmU4MzdjY2RmZjU3MzE="
+  );
+
+export const setLinks = async (links: PinboardItem[]) => {
+  authorizeUpstash();
+  await set("unread", JSON.stringify(links), "ex", 30);
+};
 
 export const fetchAll = async () => {
-  const links = await pinboard.all();
-  client.del("unread").then(() => {
-    client.set("unread", JSON.stringify(links), "ex", 30);
-  });
-  return links;
+  return await pinboard.all();
+};
+
+const updateCache = async () => {
+  authorizeUpstash();
+  const all: PinboardItem[] = await fetchAll();
+  setLinks(all.filter((v) => v.toread === "yes"));
 };
 
 export const fetchUnread = async () => {
-  const all = await fetchAll();
-  return all.filter((v: { toread: "yes" | "no" }) => v.toread === "yes");
+  authorizeUpstash();
+  updateCache();
+  return JSON.parse((await get("unread")).data);
 };
 
 export default async function handler(
