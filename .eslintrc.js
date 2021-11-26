65ad21c806e306bd813251baa72a071bae107c60
Brandon Pittman <brandon@blp.is>
2021-11-26 16:05:28 +0900

    remove unused deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,20 +19,6 @@ module.exports = {
     "plugin:prettier/recommended", // Make sure this is always the last element in the array.
   ],
   rules: {
-    // "prettier/prettier": ["error", {}, { usePrettierrc: true }],
-    // "react/display-name": "off",
-    // "prefer-const": "off",
-    // "@typescript-eslint/explicit-function-return-type": "off",
-    // "@typescript-eslint/explicit-module-boundary-types": "off",
-    // "@typescript-eslint/no-explicit-any": "off",
     "react/no-unescaped-entities": "off",
-    // "jsx-a11y/anchor-is-valid": [
-    //   "error",
-    //   {
-    //     components: ["Link"],
-    //     specialLink: ["hrefLeft", "hrefRight"],
-    //     aspects: ["invalidHref", "preferButton"],
-    //   },
-    // ],
   },
 };
