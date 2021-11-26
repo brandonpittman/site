0ac4b4f0cded178cd616f4c5e6f08da234c00138
Brandon Pittman <brandon@blp.is>
2021-11-26 15:01:50 +0900

    update husky stuf

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   "**/*.(ts?(x)|?(m)js)": (filenames) => [
-    `next lint --fix --file ${filenames
-      .map((file) => file.split(process.cwd())[1])
-      .join(" --file ")}`,
+    // `next lint --fix --file ${filenames
+    //   .map((file) => file.split(process.cwd())[1])
+    //   .join(" --file ")}`,
     `prettier --write ${filenames.join(" ")}`,
   ],
 };
