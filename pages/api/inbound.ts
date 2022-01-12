7892f4fa632236b0f49f3f1fc75c476dd886ffae
Brandon Pittman <brandon@blp.is>
2022-01-12 09:52:24 +0900

    log req.body

--- pages/api/inbound.ts
+++ pages/api/inbound.ts
@@ -4,7 +4,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req);
+  console.log(req.body);
   res.status(200).json({
     message: "OK",
   });
