31cc00d577c5c0ec55cb5842a09105b746dc2ab6
Brandon Pittman <brandon@blp.is>
2022-01-12 10:05:17 +0900

    log keys

--- pages/api/inbound.ts
+++ pages/api/inbound.ts
@@ -4,7 +4,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log({ keys: Object.keys(req) });
+  console.log({ keys: Object.keys(req.body) });
   res.status(200).json({
     message: "OK",
   });
