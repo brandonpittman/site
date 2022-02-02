a2f48435524751b4a0359ccb687ae5036762f91f
Brandon Pittman <brandon@blp.is>
2022-02-02 10:12:06 +0900

    Use GitHub action to cache links

--- pages/api/pinboard/cache-links.ts
+++ pages/api/pinboard/cache-links.ts
@@ -1,10 +1,5 @@
-import { CronJob } from "quirrel/next";
 import { updateCache } from "./unread";
 
-export default CronJob(
-  "api/pinboard/cache-links",
-  ["0 7-23/2 * * *", "Asia/Tokyo"],
-  async () => {
-    await updateCache();
-  }
-);
+export default async function handler() {
+  await updateCache();
+}
