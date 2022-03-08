59384f38bd8e3abc394ed19c460ba4d7f91ba03e
Brandon Pittman <brandon@blp.is>
2022-03-08 21:52:56 +0900

    Move Ukraine stripe up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,10 +16,10 @@ export default function App({ Component, pageProps }) {
     <>
       <Ogp />{" "}
       <MDXProvider>
-        <Component {...pageProps} />
         <div className="h-8 w-screen bg-[linear-gradient(180deg,#005BBB_49.9%,#FFD500_50%)]">
           <p className="sr-only">Long live, Ukraine.</p>
         </div>
+        <Component {...pageProps} />
       </MDXProvider>
     </>
   );
