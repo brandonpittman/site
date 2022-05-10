4d3a2c08cf1c1229f912da4f1e53fb370986c5e4
Brandon Pittman <brandon@blp.is>
2022-05-11 08:34:36 +0900

    update remix-pages-context

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "@remix-run/dev/server-build";
+import { getLoadContext } from "remix-pages-context";
 
 const handleRequest = createPagesFunctionHandler({
   build,
   mode: process.env.NODE_ENV,
-  getLoadContext: ({ data, env }) => {
-    return {
-      ...data,
-      ...env,
-    };
-  },
+  getLoadContext,
 });
 
 export function onRequest(context) {
