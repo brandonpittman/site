8f3345d670786110c88616cd724df4a3a58555ba
Brandon Pittman <brandon@blp.is>
2022-05-17 10:31:34 +0900

    Cache assets

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,9 @@
 import { hydrate } from "react-dom";
 import { RemixBrowser } from "@remix-run/react";
+import { cacheAssets } from "remix-utils";
+
+cacheAssets().catch((error) => {
+  console.error(error);
+});
 
 hydrate(<RemixBrowser />, document);
