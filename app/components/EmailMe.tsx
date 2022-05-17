be2477345480dfbfc02bf90c05f82fcb591fafe6
Brandon Pittman <brandon@blp.is>
2022-05-17 15:36:26 +0900

    swap in framer-motion

--- app/components/EmailMe.tsx
+++ app/components/EmailMe.tsx
@@ -1,31 +1,35 @@
-import { animate } from "motion";
+import { useAnimation, motion } from "framer-motion";
 import { useHydrated } from "remix-utils";
 
 import type { MouseEventHandler } from "react";
 
-const onClick: MouseEventHandler = ({ target }) => {
-  navigator.clipboard
-    .writeText("hey@blp.is")
-    .then(() => {
-      animate(
-        target as HTMLButtonElement,
-        {
-          opacity: [0.5, 1],
-        },
-        { duration: 1 }
-      );
-    })
-    .catch(console.error);
-};
+const EMAIL = "hey@blp.is";
 
 const CopyButton = () => {
+  const controls = useAnimation();
+
+  const onClick: MouseEventHandler = () => {
+    navigator.clipboard
+      .writeText(EMAIL)
+      .then(() => {
+        controls.start(
+          {
+            opacity: [0.5, 1],
+          },
+          { duration: 1 }
+        );
+      })
+      .catch(console.error);
+  };
+
   return (
-    <button
+    <motion.button
       onClick={onClick}
       className="bg-gray-900 text-sm text-white font-medium rounded flex py-1 px-2"
+      animate={controls}
     >
       Copy
-    </button>
+    </motion.button>
   );
 };
 
@@ -37,7 +41,7 @@ export const EmailMe = () => {
       <p>
         Email me at{" "}
         <a href="mailto:hey@blp.is" className="leading-none">
-          hey@blp.is
+          {EMAIL}
         </a>
       </p>
       {isHydrated ? <CopyButton /> : null}
