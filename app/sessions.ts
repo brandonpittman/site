05a3d7df7d306abce48a57ac54cc0a42b2885226
Brandon Pittman <brandon@blp.is>
2022-06-04 22:04:44 +0900

    Fix session_secret

--- app/sessions.ts
+++ app/sessions.ts
@@ -1,24 +1,51 @@
+import type { Session } from "@remix-run/cloudflare";
 import { createCookieSessionStorage } from "@remix-run/cloudflare";
+
+import { getPagesContext } from "remix-pages-context";
 
 const SECONDS_IN_DAY = 86400;
 const MONTH = SECONDS_IN_DAY * 31;
 let expires = new Date(Date.now() + MONTH * 1000);
 let maxAge = MONTH;
 
-let createSession = () => {
+// @see: https://github.com/jacob-ebey/remix-cloudflare-worker-template/blob/main/packages/remix-app/app/session.server.ts
+function getSessionStorage() {
+  let env = getPagesContext();
+
+  if (!env.SESSION_SECRET) throw new Error("SESSION_SECRET is not defined");
+
   return createCookieSessionStorage({
     cookie: {
       name: "__session",
       expires,
       maxAge,
-      secrets: ["6067c335efc94821b61f2540f5cb5459"],
+      secrets: [env.SESSION_SECRET],
       httpOnly: true,
       sameSite: "lax",
       secure: true,
     },
   });
-};
+}
 
-const { getSession, commitSession, destroySession } = createSession();
+export function commitSession(session: Session) {
+  let sessionStorage = getSessionStorage();
 
-export { getSession, commitSession, destroySession };
+  return sessionStorage.commitSession(session);
+}
+
+export function destroySession(session: Session) {
+  let sessionStorage = getSessionStorage();
+
+  return sessionStorage.destroySession(session);
+}
+
+export function getSession(requestOrCookie: Request | string | null) {
+  let cookie =
+    typeof requestOrCookie === "string"
+      ? requestOrCookie
+      : requestOrCookie?.headers.get("Cookie");
+
+  let sessionStorage = getSessionStorage();
+
+  return sessionStorage.getSession(cookie);
+}
