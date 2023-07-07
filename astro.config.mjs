c5d8fa107eb9170c6754e7d13b263e03d104f7b5
Brandon Pittman <brandon@blp.is>
2023-07-07 20:34:33 +0900

    Convert island to Solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 import prefetch from "@astrojs/prefetch";
-import preact from "@astrojs/preact";
-
-import svelte from "@astrojs/svelte";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://brandonpittman.com/",
-  integrations: [sitemap(), prefetch(), preact(), svelte()],
+  integrations: [sitemap(), prefetch(), solidJs()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
+
--- src/islands/CopyEmail.tsx
+++ src/islands/CopyEmail.tsx
@@ -1,27 +1,27 @@
-import { useCallback, useState } from "preact/hooks";
+import { createSignal } from "solid-js";
 
 export let CopyEmail = () => {
-  let [isAnimated, setIsAnimated] = useState(false);
+  let [isAnimated, setIsAnimated] = createSignal(false);
 
-  let onClick = useCallback(() => {
+  let onClick = () => {
     navigator.clipboard.writeText("hey@brandonpittman.com").then(() => {
       setIsAnimated(true);
     });
-  }, []);
+  };
 
   return (
     <button
       onClick={onClick}
       onAnimationEnd={() => setIsAnimated(false)}
       style={
-        isAnimated
+        isAnimated()
           ? {
               animation: "var(--animation-blink)",
-              animationIterationCount: "var(--iteration-count)",
+              "animation-iteration-count": "var(--iteration-count)",
             }
           : {}
       }
-      className="cta"
+      class="cta"
     >
       Copy
     </button>
