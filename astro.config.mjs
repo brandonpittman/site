4d31e33fc064e0e3d88b9f52a16d1c0aa2f28c95
Brandon Pittman <brandon@blp.is>
2023-07-07 15:55:43 +0900

    Add Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 import prefetch from "@astrojs/prefetch";
-
 import preact from "@astrojs/preact";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://brandonpittman.com/",
-  integrations: [sitemap(), prefetch(), preact()],
+  integrations: [sitemap(), prefetch(), preact(), svelte()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
-
---
+++ src/islands/CopyEmail.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+export let isAnimated = false
+
+export let onClick = () => {
+  navigator.clipboard.writeText("hey@brandonpittman.com").then(() => {
+    isAnimated = true
+  })}
+</script>
+
+<button
+  on:click={onClick}
+  on:animationend={() => {
+    isAnimated = false
+  }}
+  style={isAnimated
+    ? "animation: var(--animation-blink); animation-iteration-count: 1"
+    : undefined
+  }
+  class="cta"
+>
+  Copy
+</button>
