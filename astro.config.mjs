76a8ad9d05cccbbc9800e4091ca445bf0a2da58c
Brandon Pittman <brandon@blp.is>
2023-07-10 09:41:04 +0900

    Show island on client only

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,19 @@ import cloudflare from "@astrojs/cloudflare";
 import prefetch from "@astrojs/prefetch";
 import solidJs from "@astrojs/solid-js";
 
+import devtools from "solid-devtools/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://brandonpittman.com/",
   integrations: [sitemap(), prefetch(), solidJs()],
   output: "server",
   adapter: cloudflare(),
+  vite: {
+    plugins: [
+      devtools({
+        autoname: true,
+      }),
+    ],
+  },
 });
-
