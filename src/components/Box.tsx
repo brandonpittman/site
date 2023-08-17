5926a4b736bb1ca3aa18a8b260d5ccb1d59ff8fd
Brandon Pittman <brandon@blp.is>
2023-08-17 13:14:21 +0900

    add atoms

---
+++ src/components/Box.tsx
@@ -0,0 +1,9 @@
+import { createBox } from "qwik-dessert";
+import { atoms } from "~/styles/atoms.css";
+
+// export const PrimaryBox = atomized("button", {
+//   color: "inverted",
+//   backgroundColor: "primary",
+// });
+
+export const Box = createBox({ atoms });
