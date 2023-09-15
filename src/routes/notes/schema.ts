21cfc56881a86697d2a8a59f7afd9aa1610593b9
Brandon Pittman <brandon@blp.is>
2023-09-15 13:13:11 +0900

    Use isoDate validator

--- src/routes/notes/schema.ts
+++ src/routes/notes/schema.ts
@@ -1,10 +1,8 @@
-import { object, string, boolean, optional, regex, minLength } from "valibot";
+import { object, string, boolean, optional, minLength, isoDate } from "valibot";
 
 export const schema = object({
   title: string([minLength(1, "Title required.")]),
   description: optional(string()),
-  date: string("Date required", [
-    regex(/^\d{4}-\d{2}-\d{2}$/, "ISO date required"),
-  ]),
+  date: string("Date required", [isoDate("ISO date required")]),
   draft: optional(boolean()),
 });
