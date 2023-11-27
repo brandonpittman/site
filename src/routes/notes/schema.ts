76b3e4af876760d9590ee4d53b580bec1a0a4f2c
Brandon Pittman <brandon@blp.is>
2023-11-27 09:31:17 +0900

    Add fallback for date in notes schema

--- src/routes/notes/schema.ts
+++ src/routes/notes/schema.ts
@@ -1,8 +1,19 @@
-import { object, string, boolean, optional, minLength, isoDate } from "valibot";
+import {
+  object,
+  string,
+  boolean,
+  optional,
+  minLength,
+  isoDate,
+  fallback,
+} from "valibot";
 
 export const schema = object({
   title: string([minLength(1, "Title required.")]),
   description: optional(string()),
-  date: string("Date required", [isoDate("ISO date required")]),
+  date: fallback(
+    string("Date required", [isoDate("ISO date required")]),
+    new Date().toISOString().substring(0, 10)
+  ),
   draft: optional(boolean()),
 });
