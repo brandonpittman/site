ccd5580760e0cbfb3f0bb893977f0458941f9151
Brandon Pittman <brandon@blp.is>
2024-06-27 12:30:33 +0900

    remove copy email component from index route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-import { CopyEmail } from "~/components/copy-email";
-
 const traditionStoicism =
   "https://traditionalstoicism.com/the-path-of-the-prokopton/";
 const github = "https://github.com/brandonpittman";
@@ -40,12 +38,23 @@ export default component$(() => {
         .
       </p>
 
-      <div id="contact" class="cluster gutter-s">
-        <div>
-          Email me at{" "}
-          <a href="mailto:hey@brandonpittman.com">hey@brandonpittman.com</a>
-        </div>
-        <CopyEmail />
+      <div id="contact" class="cluster gutter-3xs align-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width={1.5}
+          stroke="currentColor"
+          style={{ blockSize: "1.5rem" }}
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+          />
+        </svg>
+
+        <a href="mailto:hey@brandonpittman.com">hey@brandonpittman.com</a>
       </div>
     </>
   );
