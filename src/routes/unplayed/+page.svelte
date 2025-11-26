bbd0edec684b9cbf37ee2c4f7c5151595349d52a
Brandon Pittman <brandon@brandonpittman.com>
2025-11-26 12:48:03 +0900

    Await all games

--- src/routes/unplayed/+page.svelte
+++ src/routes/unplayed/+page.svelte
@@ -2,42 +2,24 @@
 	import GameList from '$lib/components/GameList.svelte';
 	import { get_game_list } from '$lib/games.remote';
 
-	const beaten = get_game_list('beaten');
-	const unbeaten = get_game_list('unbeaten');
-	const unplayed = get_game_list('unplayed');
-	const abandoned = get_game_list('abandoned');
+	const beaten = await get_game_list('beaten');
+	const unbeaten = await get_game_list('unbeaten');
+	const unplayed = await get_game_list('unplayed');
+	const abandoned = await get_game_list('abandoned');
 </script>
 
-{#await unplayed then data}
-	<h1>Unplayed</h1>
+<h2>Unplayed</h2>
+<small>A shortlist for potential future play.</small>
+<GameList game_list={unplayed!} />
 
-	<small>A shortlist for potential future play.</small>
+<h2>Unbeaten</h2>
+<small>But not abandoned. Yet. Yet?</small>
+<GameList game_list={unbeaten!} />
 
-	{#if data}
-		<GameList game_list={data} />
-	{/if}
-{/await}
+<h2>Beaten</h2>
+<small>Some more thoroughly—or more times—than others.</small>
+<GameList game_list={beaten!} />
 
-{#await unbeaten then data}
-	<h1>Unbeaten</h1>
-	<small>But not abandoned. Yet. Yet?</small>
-	{#if data}
-		<GameList game_list={data} />
-	{/if}
-{/await}
-
-{#await beaten then data}
-	<h1>Beaten</h1>
-	<small>Some more thoroughly—or more times—than others.</small>
-	{#if data}
-		<GameList game_list={data} />
-	{/if}
-{/await}
-
-{#await abandoned then data}
-	<h1>Abandoned</h1>
-	<small>Not necessarily bad, just maybe not for me.</small>
-	{#if data}
-		<GameList game_list={data} />
-	{/if}
-{/await}
+<h2>Abandoned</h2>
+<small>Not necessarily bad, just maybe not for me.</small>
+<GameList game_list={abandoned!} />
