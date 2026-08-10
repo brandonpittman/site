593cc6d01b26fc487e47a15a2dc36860241b5ecf
Brandon Pittman <brandon@brandonpittman.com>
2026-08-10 13:08:32 +0900

    Remove cruft

--- src/hooks.server.ts
+++
@@ -1,6 +0,0 @@
-// export const handle = async ({ event, resolve }) => {
-// 	const response = await resolve(event);
-// 	// Block Cloudflare scripts
-// 	response.headers.set('cache-control', 'public, max-age=0, must-revalidate, no-transform');
-// 	return response;
-// };
