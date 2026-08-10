14591d24e189b2532b62e97d2f747e6ff9c4484e
Brandon Pittman <brandon@brandonpittman.com>
2026-08-10 12:33:43 +0900

    Disable hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
-export const handle = async ({ event, resolve }) => {
-	const response = await resolve(event);
-	// Block Cloudflare scripts
-	response.headers.set('cache-control', 'public, max-age=0, must-revalidate, no-transform');
-	return response;
-};
+// export const handle = async ({ event, resolve }) => {
+// 	const response = await resolve(event);
+// 	// Block Cloudflare scripts
+// 	response.headers.set('cache-control', 'public, max-age=0, must-revalidate, no-transform');
+// 	return response;
+// };
